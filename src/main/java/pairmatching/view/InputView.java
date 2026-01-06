11caf51f8eca57d9bf41e69300798cac5f2d77d4
softmoca <acyc0909@gmail.com>
2026-01-06 18:17:20 +0900

    refactor : inputview에서의 파싱 로직 추출

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,10 +1,8 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import pairmatching.util.Parser;
 
 public class InputView {
     public String readFunction() {
@@ -22,23 +20,8 @@ public class InputView {
         System.out.println("과정, 레벨, 미션을 선택하세요.");
         System.out.println("ex) 백엔드, 레벨1, 자동차경주");
         String input = Console.readLine();
-        return splitAndTrimValidate(input, ",");
+        return Parser.splitAndTrimValidate(input, ",");
     }
-
-    public List<String> splitAndTrimValidate(String line, String delimiter) {
-        List<String> tokens = Arrays.stream(line.split(Pattern.quote(delimiter), -1))
-                .map(String::trim)
-                .collect(Collectors.toList());
-        validateNotBlank(tokens);
-        return tokens;
-    }
-
-    private void validateNotBlank(List<String> tokens) {
-        if (tokens.stream().anyMatch(String::isEmpty)) {
-            throw new IllegalArgumentException("[ERROR] 과정과 미션 입력");
-        }
-    }
-
 
     public String readReMatchingAnswer() {
         System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
