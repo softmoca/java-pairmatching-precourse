42c5b919bfeafbc68be322507bb92274c1afd955
softmoca <acyc0909@gmail.com>
2026-01-07 14:44:52 +0900

    refactor(OutputView): 출력 문구 상수화 및 포맷 상수 정리

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,44 +1,46 @@
 package pairmatching.view;
 
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 import pairmatching.domain.Pair;
 
 public class OutputView {
-    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(Locale.KOREA);
     private static final String NEW_LINE = System.lineSeparator();
 
+    private static final String LINE_SEPARATOR = "#############################################";
+    private static final String COURSE_LINE = "과정: 백엔드 | 프론트엔드";
+    private static final String MISSION_TITLE = "미션:";
+    private static final String LEVEL1_LINE = "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임";
+    private static final String LEVEL2_LINE = "  - 레벨2: 장바구니 | 결제 | 지하철노선도";
+    private static final String LEVEL3_LINE = "  - 레벨3: ";
+    private static final String LEVEL4_LINE = "  - 레벨4: 성능개선 | 배포";
+    private static final String LEVEL5_LINE = "  - 레벨5: ";
 
-    public void printCourseAndMission() {//TODO
-        System.out.println("#############################################");
-        System.out.println("과정: 백엔드 | 프론트엔드");
-        System.out.println("미션:");
-        System.out.println("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임");
-        System.out.println("  - 레벨2: 장바구니 | 결제 | 지하철노선도");
-        System.out.println("  - 레벨3: ");
-        System.out.println("  - 레벨4: 성능개선 | 배포");
-        System.out.println("  - 레벨5: ");
-        System.out.println("#############################################");
+    public void printCourseAndMission() {
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(COURSE_LINE);
+        System.out.println(MISSION_TITLE);
+        System.out.println(LEVEL1_LINE);
+        System.out.println(LEVEL2_LINE);
+        System.out.println(LEVEL3_LINE);
+        System.out.println(LEVEL4_LINE);
+        System.out.println(LEVEL5_LINE);
+        System.out.println(LINE_SEPARATOR);
 
     }
+
 
     public void printMatchingResult(List<Pair> pairList) {
         System.out.println("페어 매칭 결과입니다.");
         for (Pair pair : pairList) {
             System.out.println(String.join(" : ", pair.getNames()));
         }
-        System.out.println();
+        System.out.print(NEW_LINE);
 
     }
 
 
     public void printError(String message) {
         System.out.println(message);
-    }
-
-    public String formatNumber(int number) {
-        return NUMBER_FORMAT.format(number);
     }
 
 }
